95e58fc44c3d4c5d5cab812d650e09c5c0d6a4b4
johnpradeep06 <161060583+johnpradeep06@users.noreply.github.com>
2024-07-26 00:54:48 +0530

    src files uploaded

---
+++ src/data/constants.js
@@ -0,0 +1,349 @@
+export const Bio = {
+  name: "Cyril praveen",
+  roles: ["Cyril praveen", "Industrial Engineer", "Tech Enthusiast", "Data Analyst"],
+  description:
+    "I consider myself a flexible and driven person who is always willing to try new things. Delivering top-notch results is my passion, and I'm eager to learn about new technologies. With a growth mindset and a positive outlook, I'm ready to make meaningful contributions and achieve great things.",
+  github: "https://github.com/sonishstark",
+  resume:
+    "https://github.com/sonishstark/Resume/blob/main/Sonish_Resume1.pdf",
+  linkedin: "https://in.linkedin.com/in/cyrilpraveen",
+  twitter: "https://twitter.com/SonishStark",
+  insta: "https://www.instagram.com/timepassguyyy/",
+  
+};
+
+export const skills = [
+  {
+    title: "Programming",
+    skills: [
+      {
+        name: "React Js",
+        image:
+          "https://raw.githubusercontent.com/sonishstark/Portfolio-website/dd18d5f4aee6db9a2275dac2cb1043e18c988ec7/public/React.svg",
+      },
+      {
+        name: "Angular Js",
+        image:
+          "https://github.com/sonishstark/Portfolio-website/blob/main/public/angular.png?raw=true",
+      },
+      {
+        name: "HTML",
+        image:
+          "https://github.com/sonishstark/Portfolio-website/blob/main/public/html.png?raw=true",
+      },
+      {
+        name: "CSS",
+        image:
+          "https://github.com/sonishstark/Portfolio-website/blob/main/public/CSS.png?raw=true",
+      },
+      {
+        name: "JavaScript",
+        image:
+          "https://github.com/sonishstark/Portfolio-website/blob/main/public/js.png?raw=true",
+      },
+      {
+        name: "Bootstrap",
+        image:
+          "https://github.com/sonishstark/Portfolio-website/blob/main/public/bootstrap.png?raw=true",
+      },
+      {
+        name: "MySQL",
+        image:
+          "https://github.com/sonishstark/Portfolio-website/blob/main/public/sql.png?raw=true",
+      },
+      {
+        name: "Node Js",
+        image:
+          "https://github.com/sonishstark/Portfolio-website/blob/main/public/node.png?raw=true",
+      },
+      {
+        name: "Python",
+        image:
+          "https://raw.githubusercontent.com/sonishstark/Portfolio-website/50ea5d5567b1ad3bdebc151bbcc5e1d1448d29be/public/python.svg",
+      },
+      {
+        name: "Java",
+        image:
+          "https://raw.githubusercontent.com/sonishstark/Portfolio-website/50ea5d5567b1ad3bdebc151bbcc5e1d1448d29be/public/java.svg",
+      },
+      {
+        name: "C/C++",
+        image:
+          "https://github.com/sonishstark/Portfolio-website/blob/main/public/c.png?raw=true",
+      },
+    ],
+  },
+  {
+    title: "Technical",
+    skills: [
+      {
+        name: "AWS",
+        image:
+          "https://github.com/sonishstark/Portfolio-website/blob/main/public/aws.jpg?raw=true",
+      },
+      {
+        name: "Git",
+        image:
+          "https://github.com/sonishstark/Portfolio-website/blob/main/public/git.png?raw=true",
+      },
+      {
+        name: "GitHub",
+        image:
+          "https://github.com/sonishstark/Portfolio-website/blob/main/public/GitHub.png?raw=true",
+      },
+      {
+        name: "Arduino",
+        image:
+          "https://github.com/sonishstark/Portfolio-website/blob/main/public/Arduino2.png?raw=true",
+      },
+      {
+        name: "Postman",
+        image:
+          "https://raw.githubusercontent.com/sonishstark/Portfolio-website/bea10f030202fb553a22c550317c355dc4a4cc4d/public/postman.svg",
+      },
+      {
+        name: "MongoDB",
+        image:
+          "https://raw.githubusercontent.com/sonishstark/Portfolio-website/bea10f030202fb553a22c550317c355dc4a4cc4d/public/mongodb.svg",
+      },
+      {
+        name: "VS Code",
+        image:
+          "https://github.com/sonishstark/Portfolio-website/blob/main/public/vscode.png?raw=true",
+      },
+      {
+        name: "PyCharm",
+        image:
+          "https://github.com/sonishstark/Portfolio-website/blob/main/public/PyCharm.png?raw=true",
+      },
+    ],
+  },
+
+  
+];
+
+export const experiences = [
+  {
+    id: 0,
+    img: "https://github.com/sonishstark/Portfolio-website/blob/main/public/Accenture.png?raw=true",
+    role: "Associate Software Engineer",
+    company: "Accenture",
+    date: "Feb 2022- June 2023",
+    desc: "As a full-stack developer, I worked with multiple frameworks and integrated React and Python scripts to create an e-commerce website for the clients. worked with AWS to manage a website and deploy billing UI for services in accordance with client specifications.",
+    skills: [
+      "Python",
+      "React",
+      "AWS",
+      "pandas",
+      "JavaScript",
+      "Node Js",
+      "Git",
+    ],
+    // doc: "https://media.licdn.com/dms/image/D4D2DAQFlp60ZqHuaFQ/profile-treasury-image-shrink_1280_1280/0/1691180828512?e=1692381600&v=beta&t=mM5Y_NE5EPlQhez5FAN6NLVSKcO_Ojt_9Gq3mnFGkAQ",
+  },
+  {
+    id: 1,
+    img: "https://github.com/sonishstark/Portfolio-website/blob/main/public/Accenture.png?raw=true",
+    role: "Stream Training",
+    company: "Accenture",
+    date: "Sept 2021 - Feb 2022",
+    desc: "I developed a user-specific pizza ordering website using Angular, incorporating customization features. Collaborated with teams using Git for version control. The training overall enhanced my knowledge on the listed skillset below. ",
+    skills: [
+      "Angular JS",
+      "Node Js",
+      "HTML",
+      "CSS",
+      "JavaScript",
+      "SQL",
+      "MongoDB",
+      "Postman API",
+    ],
+  },
+];
+
+export const education = [
+  {
+    id: 0,
+    img: "https://github.com/sonishstark/Portfolio-website/blob/main/public/GW.jpg?raw=true",
+    school: "The George Washington University",
+    date: "Aug 2023 - May 2025",
+    desc: "I am currently pursuing my Master’s in Computer Science program which reflects a strong foundation in computer science and a keen interest in cutting-edge technologies. In the first semester, I delved into design analysis of algorithms, understanding the core principles of efficient problem-solving. Exploring computer system architecture provided insights into hardware-software interactions crucial for system design. Advanced software paradigms expanded my knowledge of modern software development techniques. Currently, I am immersing myself in the realms of machine learning, big data, and database management, aiming to leverage data-driven insights for impactful decision-making. This diverse skill set equips me to tackle complex challenges, from optimizing algorithms to designing scalable systems and harnessing the power of data for intelligent solutions, exploring AWS platforms. I am passionate about pushing the boundaries of technology and contributing meaningfully to the field of computer science and data analytics.",
+    degree:
+      "Masters in Computer Science - (Machine intelligence and cognition)",
+  },
+  {
+    id: 1,
+    img: "https://github.com/sonishstark/Portfolio-website/blob/main/public/sathyabama.jpg?raw=true",
+    school: "Sathyabama Institute of Science and Technology",
+    date: "Aug 2017 - May 2021",
+    desc: "As an avid learner and enthusiast of computer science, my education portfolio showcases a diverse range of expertise. I have delved into data structures and object-oriented programming, honing my skills in creating efficient algorithms and developing robust software systems. My experience in the Microprocessor & Microcontroller Lab has equipped me with hands-on knowledge of embedded systems and hardware programming. Operating systems and networking labs have allowed me to understand the intricate workings of computer networks and the core principles behind efficient resource management. I have explored the realms of computer graphics and multimedia, merging creativity with technology to create visually engaging and interactive experiences. Additionally, my studies in data mining & data warehousing have sharpened my ability to extract valuable insights from large datasets, contributing to informed decision-making processes. Moreover, I have gained practical insights into mobile application development, integrating user-friendly interfaces with powerful functionalities. This comprehensive education journey reflects my passion for leveraging technology to solve real-world problems and innovate for the future.",
+    degree: "Bachelor of Engineering - B.E, Computer Science and Engineering",
+  },
+  {
+    id: 2,
+    img: "https://github.com/sonishstark/Portfolio-website/blob/main/public/doveton.png?raw=true",
+    school: "Doveton Boys Higher Secondary School",
+    date: " Jun 2015 - May 2017",
+    desc: "Acquired expertise in Computer Science programming languages (Java, C, C++) and learned Advanced Mathematics, Physics, and Chemistry. Certified with 5 years of Social Useful Productive Work (SUPW) and 2 years of Road Safety Patrol (RSP) with the engagement of community service involvement. Actively involved in the Athletics Department, serving as the school house representative for five years and taking home gold medals in the 12th-grade long jump, 12th-grade sprint 4*100, and 12th-grade high jump competitions.",
+    degree: "ICSC(XII), Science with Computer",
+  },
+];
+
+
+
+export const projects = [
+  
+  {
+    id: 0,
+    title: "Weather Forecast",
+    date: "Sept 2023 - Nov 2023",
+    description:
+      "Using the Kemal framework and the Crystal programming language, we build a weather prediction website that can be used to track the weather in a specific area and be adapted to our daily lives. Features include real-time weather data, rainfall predictions, geolocation selection, weather alerts, and agricultural insights to help with planning based on the predicted data.",
+    image:
+      "https://github.com/sonishstark/Portfolio-website/blob/main/public/Weather.png?raw=true",
+    tags: ["Cystal", "Kemal", "JavaScript", "HTML", "CSS"],
+    category: "web app",
+    github: "https://github.com/pradhyuman-yadav/csci_6221",
+    member: [
+      {
+        name: "Pradhyuman Yadav",
+        img: "https://avatars.githubusercontent.com/u/44358290?v=4",
+        linkedin: "https://www.linkedin.com/in/pradhyuman-yadav/",
+        github: "https://github.com/pradhyuman-yadav",
+      },
+      {
+        name: "Sonish Sasidharan",
+        img: "https://avatars.githubusercontent.com/u/52243863?v=4",
+        linkedin: "https://www.linkedin.com/in/sonish-s/",
+        github: "https://github.com/sonishstark",
+      },
+    ],
+  },
+  {
+    id: 1,
+    title: "Group Media",
+    date: "Jan 2021 - May 2021",
+    description:
+      "Developed a media website with HTML and JavaScript that allows users to explore more than 40 groups for socializing and Networking. It is an open-space website featuring several organized groups and their interacting data. The goal of this platform was for the users to share knowledge, information and communicate among themselves.",
+    image:
+      "https://github.com/sonishstark/Portfolio-website/blob/main/public/Gorup_media.jpg?raw=true",
+    tags: ["JavaScript", "Django", "Bootstrap", "HTML", "CSS", "Apache Tomcat"],
+    category: "web app",
+    github: "https://github.com/sonishstark/Group_Media",
+    member: [
+      {
+        name: "Vivian Xavier",
+        img: "https://1000logos.net/wp-content/uploads/2021/05/Fallen-logo.png",
+        linkedin: "https://www.linkedin.com/in/vivian-xavier-460899189/",
+      },
+      {
+        name: "Sonish Sasidharan",
+        img: "https://avatars.githubusercontent.com/u/52243863?v=4",
+        linkedin: "https://www.linkedin.com/in/sonish-s/",
+        github: "https://github.com/sonishstark",
+      },
+    ],
+  },
+  {
+    id: 2,
+    title: "Snake Game",
+    date: "Jul 2020 - Aug 2020",
+    description:
+      "created a 2D-panel game with PyCharm and Python that has a user-interface (UI) enabling the users to immerse in an interactive gaming experience using the small pop panel.",
+    image:
+      "https://github.com/sonishstark/Portfolio-website/blob/main/public/snake_game.png?raw=true",
+    tags: ["Python", "PyCharm", "JetBrains", "Turtle", "Unity"],
+    category: "others",
+    github: "https://github.com/sonishstark/Snake_Game",
+    member: [
+      {
+        name: "Sonish Sasidharan",
+        img: "https://avatars.githubusercontent.com/u/52243863?v=4",
+        linkedin: "https://www.linkedin.com/in/sonish-s/",
+        github: "https://github.com/sonishstark",
+      },
+    ],
+  },
+  {
+    id: 3,
+    title: "AI sign detection",
+    date: "Dec 2019 (Internship)",
+    description:
+      "Developed AI for detecting hand signs, discovered several methods for interpreting data by executing tasks in an intelligent manner, and gained an understanding of how cognitive AI functions work based on deep and reinforcement learning.",
+    image:
+      "https://github.com/sonishstark/Portfolio-website/blob/main/public/hand_sign.jpg?raw=true",
+    tags: ["Python", "TensorFLow", "Neural Network", "Keras"],
+    category: "others",
+    github:
+      "https://github.com/sonishstark/Object_Detection/blob/main/AI_Certificate.pdf",
+    member: [
+      {
+        name: "Sonish Sasidharan",
+        img: "https://avatars.githubusercontent.com/u/52243863?v=4",
+        linkedin: "https://www.linkedin.com/in/sonish-s/",
+        github: "https://github.com/sonishstark",
+      },
+    ],
+  },
+  {
+    id: 10,
+    title: "Object Detection",
+    date: "Dec 2018 (Internship)",
+    description:
+      "Created a proximity tool with a sensor that can recognize objects in motion and determine how close they are, so that the user can be aware it  also developed the ability to record substances in front of it and move accordingly to avoid obstruction.",
+    image:
+      "https://github.com/sonishstark/Portfolio-website/blob/main/public/Arduino.png?raw=true",
+    tags: ["Arduino", "Ultrasonic Sensor", "C/C++", "HC-SR04"],
+    category: "others",
+    github:
+      "https://github.com/sonishstark/Object_Detection/blob/main/UltraSonic_certificate.pdf",
+    member: [
+      {
+        name: "Sonish Sasidharan",
+        img: "https://avatars.githubusercontent.com/u/52243863?v=4",
+        linkedin: "https://www.linkedin.com/in/sonish-s/",
+        github: "https://github.com/sonishstark",
+      },
+    ],
+  },
+];
+
+export const TimeLineData = [
+  { year: 2017, text: "Started my journey" },
+  { year: 2018, text: "Worked as a freelance developer" },
+  { year: 2019, text: "Founded JavaScript Mastery" },
+  { year: 2020, text: "Shared my projects with the world" },
+  { year: 2021, text: "Started my own platform" },
+];
+
+export const achievement = [
+  
+    {
+      id: 0,
+      title: "Weather Forecast",
+      date: "Sept 2023 - Nov 2023",
+      description:
+        "Using the Kemal framework and the Crystal programming language, we build a weather prediction website that can be used to track the weather in a specific area and be adapted to our daily lives. Features include real-time weather data, rainfall predictions, geolocation selection, weather alerts, and agricultural insights to help with planning based on the predicted data.",
+      image:
+        "https://github.com/sonishstark/Portfolio-website/blob/main/public/Weather.png?raw=true",
+      tags: ["Cystal", "Kemal", "JavaScript", "HTML", "CSS"],
+      category: "web app",
+      github: "https://github.com/pradhyuman-yadav/csci_6221",
+      member: [
+        {
+          name: "Pradhyuman Yadav",
+          img: "https://avatars.githubusercontent.com/u/44358290?v=4",
+          linkedin: "https://www.linkedin.com/in/pradhyuman-yadav/",
+          github: "https://github.com/pradhyuman-yadav",
+        },
+        {
+          name: "Sonish Sasidharan",
+          img: "https://avatars.githubusercontent.com/u/52243863?v=4",
+          linkedin: "https://www.linkedin.com/in/sonish-s/",
+          github: "https://github.com/sonishstark",
+        },
+      ],
+    },
+
+  
+]
